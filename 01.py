7e851b32bfc036731f385440b2b5aecb58459f61
chbskzzz <chbsk.nice@gmail.com>
2023-07-11 22:21:39 +0900

    init

---
+++ 01.py
@@ -0,0 +1,30 @@
+#6
+print("Welcome to the band name generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
+
+exit()
+#5
+name = input("What is your name?")
+print(name)
+
+exit()
+
+#4
+# print("Hello " + input("What is your name?"))
+print(len(input("What is your name?")))
+
+
+exit()
+#1
+print('Day1 1- Python Print Function')
+print("The function is declared like this:")
+print("print('what to print')")
+
+#2
+print("Hellow world!\nHellow world!")
+exit()
+
+#3
+print("Hello"+"Angel")
