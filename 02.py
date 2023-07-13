9f98115b33c62376bfde10b4991af46302872597
chbskzzz <chbsk.nice@gmail.com>
2023-07-13 22:34:45 +0900

    init

---
+++ 02.py
@@ -0,0 +1,8 @@
+# String, Integer, Float, Boolean
+
+# BMI
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = int(weight) / float(height) ** 2
+print(bmi)
---
+++ 03.py
@@ -0,0 +1,62 @@
+str1 = "LOVE"
+print(str1)
+lower_case_string = str1.lower()
+print(lower_case_string)
+t = lower_case_string.count("t")
+l = lower_case_string.count("l")
+
+tl = t + l
+
+print(tl)
+
+
+exit()
+
+
+'''
+if condition:
+    do this
+else:
+    do this
+'''
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?   "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+
+'''
+Nested if / else
+if condition:
+    if another condition:
+        do this
+    else:
+        do this
+else:
+    do this
+'''
+
+'''
+if / elif / else
+if condition1:
+    do A
+elif:
+    do B
+else:
+    do this
+'''
+
+'''
+if condition1 & condition2 & condition3:
+    do this
+else:
+    do this
+    
+LOGICAL OPERATORS
+    A and B
+    C or D
+    not E
+'''
